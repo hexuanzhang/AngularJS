af17381520dd221c16a562ccd8b8c197acbf5a2d
hexuanzhang <hexuan.zhang@pingxx.com>
2016-09-26 16:55:10 +0800

    $eval & $evalAsync 用法

---
+++ code/eval/eval.js
@@ -0,0 +1,25 @@
+angular.module("ngEval", [])
+    .controller("eval", evalController);
+
+evalController.$inject = ["$log", "$scope"];
+
+function evalController($log, $scope) {
+    var e = this;
+
+    $scope.a = 1;
+    $scope.b = 2;
+
+    console.log("eval", new Date().toLocaleString(), $scope.$eval('a+b'));  // 3
+    console.log("eval", new Date().toLocaleString(), $scope.$eval(function(scope){ return scope.a + scope.b; })); // 3
+
+    e.name = "evalAsync demo";
+
+    document.getElementById("async_btn").onclick = function() {
+        $scope.$evalAsync(function(scope) {
+            $log.info(scope === $scope);
+            e.name = "evalAsync test";
+        })
+    };
+
+
+}
